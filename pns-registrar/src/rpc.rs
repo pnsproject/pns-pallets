810fcb20732d88a57b4cda4e291b53d37b5bc5c6
Cupnfish <cupnfish@icloud.com>
2021-11-15 16:05:40 +0800

    Update rpc

--- pns-registrar/src/rpc.rs
+++ pns-registrar/src/rpc.rs
@@ -3,17 +3,20 @@ use sp_runtime::traits::MaybeDisplay;
 use sp_std::vec::Vec;
 
 sp_api::decl_runtime_apis! {
-    pub trait PnsRpcApi<AccountId, Node, Balance, Len, NodeState>
+    pub trait PnsRpcApi<AccountId, Node, Balance, Duration>
     where AccountId: Codec + MaybeDisplay,
     Node: Codec + MaybeDisplay,
     Balance: Codec + MaybeDisplay,
-    Len: Codec + MaybeDisplay,
-    NodeState: Codec + MaybeDisplay,
+    Duration: Codec + MaybeDisplay,
     {
         fn query_nodes(owner: AccountId) -> Vec<Node>;
-        fn query_price(len: Len) -> Balance;
+        fn renew_price(name_len: u8, duration: Duration) -> Balance;
+        fn registry_price(name_len: u8, duration: Duration) -> Balance;
+        fn register_fee(name_len: u8) -> Balance;
         fn query_operators(caller: AccountId) -> Vec<AccountId>;
-        fn query_expires(node: Node) -> NodeState;
+        fn check_expires_registrable(node: Node) -> bool;
+        fn check_expires_renewable(node: Node) -> bool;
+        fn check_expires_useable(node: Node) -> bool;
         fn query_subnode(node:Node,label:Node) -> Node;
     }
 }
