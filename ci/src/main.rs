1f8fe65cee95849c944fdfce92f6079546d34af5
Cupnfish <cupnfish@icloud.com>
2022-03-18 12:15:44 +0800

    Update `xshell`

--- ci/src/main.rs
+++ ci/src/main.rs
@@ -1,30 +1,37 @@
-use xshell::cmd;
+use xshell::{cmd, Shell};
 
 fn main() {
+    let sh = Shell::new().expect("Shell create field.");
     // When run locally, results may differ from actual CI runs triggered by
     // .github/workflows/ci.yml
     // - Official CI runs latest stable
     // - Local runs use whatever the default Rust is locally
 
     // See if any code needs to be formatted
-    cmd!("cargo fmt --all -- --check")
+    cmd!(sh, "cargo fmt --all -- --check")
         .run()
         .expect("Please run 'cargo fmt --all' to format your code.");
 
     // See if clippy has any complaints.
     // - Type complexity must be ignored because we use huge templates for queries
-    cmd!("cargo clippy --workspace --all-targets --all-features -- -D warnings -A clippy::type_complexity -W clippy::doc_markdown")
+    cmd!(sh,"cargo clippy --workspace --all-targets --all-features -- -D warnings -A clippy::type_complexity -W clippy::doc_markdown")
         .run()
         .expect("Please fix clippy errors in output above.");
 
-    cmd!("cargo clippy --no-default-features --features runtime-benchmarks")
-        .run()
-        .expect("Please fix clippy errors in output above.");
+    cmd!(
+        sh,
+        "cargo clippy --no-default-features --features runtime-benchmarks"
+    )
+    .run()
+    .expect("Please fix clippy errors in output above.");
 
     // Cargo check
-    cmd!("cargo check --package pns-resolvers --lib --all-features")
-        .run()
-        .expect("Please fix check errors in output above.");
+    cmd!(
+        sh,
+        "cargo check --package pns-resolvers --lib --all-features"
+    )
+    .run()
+    .expect("Please fix check errors in output above.");
 
     // These tests are already run on the CI
     // Using a double-negative here allows end-users to have a nicer experience
@@ -32,12 +39,12 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.get(1) != Some(&"nonlocal".to_string()) {
         // Run tests
-        cmd!("cargo test --workspace")
+        cmd!(sh, "cargo test --workspace")
             .run()
             .expect("Please fix failing tests in output above.");
 
         // Run doc tests: these are ignored by `cargo test`
-        cmd!("cargo test --doc --workspace")
+        cmd!(sh, "cargo test --doc --workspace")
             .run()
             .expect("Please fix failing doc-tests in output above.");
     }
